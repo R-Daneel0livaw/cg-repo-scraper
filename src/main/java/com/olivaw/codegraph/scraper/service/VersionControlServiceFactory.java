2bbe193135272764dd60f80ebf2b1201b6682572
erich <eric.hine2214@gmail.com>
2024-11-29 14:27:14 -0500

    Add structure for Version Control Controller.

--- src/main/java/com/olivaw/codegraph/scraper/service/VersionControlServiceFactory.java
+++ src/main/java/com/olivaw/codegraph/scraper/service/VersionControlServiceFactory.java
@@ -5,8 +5,6 @@ public class VersionControlServiceFactory {
     public static VersionControlService getService(String url) {
         if (url.contains("github.com")) {
             return new GitHubService();
-        } else if (url.contains("gitlab.com")) {
-            return new GitLabService();
         } else if (url.startsWith("file://")) {
             return new LocalFileSystemService();
         } else {
