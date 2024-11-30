6c2ed10e472112a9bcbbb61ab45846b06c6ad8c2
erich <eric.hine2214@gmail.com>
2024-11-30 16:32:40 -0500

    Add JGit as a dependency for Git tasks.

--- src/main/java/com/olivaw/codegraph/scraper/service/GitHubService.java
+++ src/main/java/com/olivaw/codegraph/scraper/service/GitHubService.java
@@ -1,13 +1,15 @@
 package com.olivaw.codegraph.scraper.service;
 
 import com.olivaw.codegraph.scraper.model.VersionControlRequest;
+import org.eclipse.jgit.api.Git;
+
 
 import java.util.List;
 
 public class GitHubService implements VersionControlService {
     @Override
     public void fetchLatestFiles(VersionControlRequest request) throws Exception {
-
+        Git.cloneRepository();
     }
 
     @Override
