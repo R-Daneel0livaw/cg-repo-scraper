65195f1ed6b5ba8878d46084969d43ba10d28d18
erich <eric.hine2214@gmail.com>
2024-12-07 15:59:44 -0500

    Centralize building the GitActionConfig into a single call.

--- src/main/java/com/olivaw/codegraph/scraper/service/GitHubService.java
+++ src/main/java/com/olivaw/codegraph/scraper/service/GitHubService.java
@@ -4,45 +4,48 @@ import com.olivaw.codegraph.scraper.model.GitActionConfig;
 import com.olivaw.codegraph.scraper.model.VersionControlRequest;
 import com.olivaw.codegraph.scraper.utils.GitUtils;
 
+import java.io.File;
 import java.util.List;
 
 public class GitHubService implements VersionControlService {
     @Override
     public void fetchLatestFiles(VersionControlRequest request) {
-        GitActionConfig config = new GitActionConfig();
-        config.setRepoLocation(request.getRepoLocation());
-        config.setTargetDirectory(null);
-        config.setDepth(1);
-
+        GitActionConfig config = getGitActionConfig(request, 1);
         GitUtils.performGitAction(config);
     }
 
     @Override
     public void fetchFullHistory(VersionControlRequest request) {
-        GitActionConfig config = new GitActionConfig();
-        config.setRepoLocation(request.getRepoLocation());
-        config.setTargetDirectory(null);
-
+        GitActionConfig config = getGitActionConfig(request);
         GitUtils.performGitAction(config);
     }
 
     @Override
     public List<String> fetchHistoryBetweenDates(VersionControlRequest request) {
-        GitActionConfig config = new GitActionConfig();
-        config.setRepoLocation(request.getRepoLocation());
-        config.setTargetDirectory(null);
-        config.setDepth(1);
+        GitActionConfig config = getGitActionConfig(request, 1);
         GitUtils.performGitAction(config, git -> {});
         return null;
     }
 
     @Override
     public String fetchDiff(VersionControlRequest request) {
-        GitActionConfig config = new GitActionConfig();
-        config.setRepoLocation(request.getRepoLocation());
-        config.setTargetDirectory(null);
-        config.setDepth(1);
+        GitActionConfig config = getGitActionConfig(request, 1);
         GitUtils.performGitAction(config, git -> {});
         return null;
+    }
+
+    private GitActionConfig getGitActionConfig(VersionControlRequest request) {
+        GitActionConfig config = new GitActionConfig();
+        config.setRepoLocation(request.getRepoLocation());
+        config.setTargetDirectory(new File("/tmp/temp-repo"));
+        return config;
+    }
+
+    private GitActionConfig getGitActionConfig(VersionControlRequest request, int depth) {
+        GitActionConfig config = new GitActionConfig();
+        config.setRepoLocation(request.getRepoLocation());
+        config.setTargetDirectory(new File("/tmp/temp-repo"));
+        config.setDepth(depth);
+        return config;
     }
 }
