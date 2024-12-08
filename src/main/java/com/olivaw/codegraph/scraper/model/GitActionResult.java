6155ce7e6d95ec47c7108ec7f45c37d62e6f7b7f
erich <eric.hine2214@gmail.com>
2024-12-08 16:18:27 -0500

    Add GitActionResult to account for returning data after performing the GitAction. Needs some cleanup to make it easier to use.

---
+++ src/main/java/com/olivaw/codegraph/scraper/model/GitActionResult.java
@@ -0,0 +1,25 @@
+package com.olivaw.codegraph.scraper.model;
+
+public class GitActionResult<T> {
+    private T data;
+    private boolean success;
+    private String message;
+
+    public GitActionResult(T data, boolean success, String message) {
+        this.data = data;
+        this.success = success;
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
