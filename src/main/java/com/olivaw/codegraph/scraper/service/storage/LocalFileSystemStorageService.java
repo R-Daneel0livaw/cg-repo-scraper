a515d9701cd32687556b5eae560407743f45a247
erich <eric.hine2214@gmail.com>
2024-12-28 16:23:27 -0500

    Add logic for store/delete local file storage.

--- src/main/java/com/olivaw/codegraph/scraper/service/storage/LocalFileSystemStorageService.java
+++ src/main/java/com/olivaw/codegraph/scraper/service/storage/LocalFileSystemStorageService.java
@@ -2,14 +2,29 @@ package com.olivaw.codegraph.scraper.service.storage;
 
 import com.olivaw.codegraph.scraper.exception.StorageException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class LocalFileSystemStorageService implements StorageService {
     @Override
     public void store(String targetPath, byte[] data) throws StorageException {
-
+        try {
+            Path path = Paths.get(targetPath);
+            Files.createDirectories(path.getParent());
+            Files.write(path, data);
+        } catch (IOException e) {
+            throw new StorageException("Failed to store data locally at " + targetPath, e);
+        }
     }
 
     @Override
     public void delete(String targetPath) throws StorageException {
-
+        try {
+            Files.deleteIfExists(Paths.get(targetPath));
+        } catch (IOException e) {
+            throw new StorageException("Failed to delete file at " + targetPath, e);
+        }
     }
 }
