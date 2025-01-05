956b262b7af6234567cbe5fbfcd7107d7c8a0343
erich <eric.hine2214@gmail.com>
2025-01-05 14:44:56 -0500

    Add Swagger.

--- src/main/java/com/olivaw/codegraph/scraper/controller/VersionControlController.java
+++ src/main/java/com/olivaw/codegraph/scraper/controller/VersionControlController.java
@@ -4,6 +4,8 @@ import com.olivaw.codegraph.scraper.model.VersionControlResponse;
 import com.olivaw.codegraph.scraper.model.request.VersionControlRequest;
 import com.olivaw.codegraph.scraper.service.retrieval.VersionControlService;
 import com.olivaw.codegraph.scraper.service.retrieval.VersionControlServiceFactory;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/vcs")
 public class VersionControlController {
 
+    @Operation(summary = "Get all files at specified location.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the files")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PostMapping("/latest")
     public ResponseEntity<VersionControlResponse> fetchLatestFiles(@RequestBody VersionControlRequest request) {
         try {
@@ -25,6 +30,9 @@ public class VersionControlController {
         }
     }
 
+    @Operation(summary = "Get all files and history at specified location.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the files")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PostMapping("/history/full")
     public ResponseEntity<VersionControlResponse> fetchFullHistory(@RequestBody VersionControlRequest request) {
         try {
@@ -36,6 +44,9 @@ public class VersionControlController {
         }
     }
 
+    @Operation(summary = "Get all files for a date range at a specified location.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the files")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PostMapping("/history/date-range")
     public ResponseEntity<VersionControlResponse> fetchHistoryBetweenDates(@RequestBody VersionControlRequest request) {
         try {
@@ -45,6 +56,9 @@ public class VersionControlController {
         }
     }
 
+    @Operation(summary = "Get all files with differences at a specified location.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the files")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PostMapping("/diff")
     public ResponseEntity<VersionControlResponse> fetchDiff(@RequestBody VersionControlRequest request) {
         try {
