7b9a236d8f0a3c31171fa937f68b212b3284f0a2
erich <eric.hine2214@gmail.com>
2025-01-12 14:46:07 -0500

    Update StorageServiceFactory to inject based on Qualifier.

--- src/main/java/com/olivaw/codegraph/scraper/service/storage/StorageServiceFactory.java
+++ src/main/java/com/olivaw/codegraph/scraper/service/storage/StorageServiceFactory.java
@@ -1,5 +1,6 @@
 package com.olivaw.codegraph.scraper.service.storage;
 
+import com.olivaw.codegraph.scraper.model.request.ResultDestinationType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -8,29 +9,22 @@ import java.util.List;
 
 public class StorageServiceFactory {
     @Autowired
-    @Qualifier("fileStorageService")
-    private StorageService<List<File>> fileStorageService;
+    @Qualifier("localFileStorageService")
+    private StorageService<List<File>> localFileStorageService;
 
     @Autowired
-    @Qualifier("stringStorageService")
-    private StorageService<List<String>> stringStorageService;
+    @Qualifier("apiStorageService")
+    private StorageService<Void> apiStorageService;
 
-//    public static StorageService getService(ResultDestinationType storageType) {
-//        return switch (storageType) {
-//            case LOCAL_STORAGE -> new LocalFileSystemStorageService();
-//            case EXTERNAL_STORAGE -> new S3StorageService();
-////                    new S3StorageService(AwsClientProvider.getS3Client(), "bucket-name");
-//            case API_RESPONSE -> new APIResponseStorageService();
-//        };
-//    }
+    @Autowired
+    @Qualifier("s3StorageService")
+    private StorageService<Void> s3StorageService;
 
-
-    public <T> StorageService<T> getService(String dataType) {
-        if ("file".equals(dataType)) {
-            return (StorageService<T>) fileStorageService; // Returning the File implementation
-        } else if ("string".equals(dataType)) {
-            return (StorageService<T>) stringStorageService; // Returning the String implementation
-        }
-        throw new IllegalArgumentException("Unsupported data type: " + dataType);
+    public <T> StorageService<T> getService(ResultDestinationType storageType) {
+        return switch (storageType) {
+            case LOCAL_STORAGE -> (StorageService<T>) localFileStorageService;
+            case EXTERNAL_STORAGE -> (StorageService<T>) s3StorageService;
+            case API_RESPONSE -> (StorageService<T>) apiStorageService;
+        };
     }
 }
